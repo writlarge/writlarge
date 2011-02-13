e036dd8ba1ca4cbc03adc1c0e798b7089a79321e
Writ Large <writlarge@nym.hush.com>
2011-02-13 15:13:28 -0500

    moved modules to sites/all/modules

--- modules/drupalchat/drupalchat.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// $Id: drupalchat.tpl.php,v 1.3 2010/09/08 09:30:33 fenda Exp $
-?>
-
-<div id="drupalchat-wrapper">
-  <div id="drupalchat">
-  	<?php print $subpanels; ?>
-  </div>
-</div>
